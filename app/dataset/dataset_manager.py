ba8dd579d11f35ec074847fabef2e8a5f3762558
Furkan Mert <mert.furkan177@gmail.com>
2026-06-29 21:19:45 +0200

    games.json and dataset_manager

---
+++ app/dataset/dataset_manager.py
@@ -0,0 +1,16 @@
+class DatasetManager:
+
+    def add_training_sample(self, sample):
+        pass
+
+    def add_review_sample(self, sample):
+        pass
+
+    def move_review_to_train(self, sample):
+        pass
+
+    def dataset_statistics(self):
+        pass
+
+    def remove_duplicates(self):
+        pass
